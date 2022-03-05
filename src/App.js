a3ffefdfd50397a6dce8145f020fb04fc5969ab6
sjk <jkshin0903@gmail.com>
2022-03-05 21:25:43 +0900

    styled components configuration + extension

--- src/App.js
+++ src/App.js
@@ -4,29 +4,30 @@ const Father = styled.div`
   display: flex;
 `
 
-const BoxOne = styled.div`
-  background-color: teal;
+/**
+ * styled component configuration은 컴포넌트 props로 구현
+ * 컴포넌트 간의 스타일 중복을 방지할 수 있다.
+ */
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`
-
-const Text = styled.span`
-  color: white;
+/**
+ * styled component extension은 styled 함수로 구현한다.
+ * 마치 typescript의 interface 확장하여 이것도 역시 스타일 정의부분의 중복을 없앨 수 있다.
+ */
+const Circle = styled(Box)`
+  border-radius: 50px;
 `
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello world!</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Box bgColor="tomato" />
+      <Circle bgColor="red" />
     </Father>
   )
 }
