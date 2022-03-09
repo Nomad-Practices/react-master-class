fcbc11c9e1bb008cbaeeb52c4f0f723fb2bac90d
sjk <jkshin0903@gmail.com>
2022-03-10 00:16:11 +0900

    google font를 적용
    styled component가 아닌 index.html head에 <link>를 inject하는 것이 권장된다.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Router from './Router'
  * index.html에 명시하지 않아도 되다니ㅠ
  */
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400&display=swap');
   html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
