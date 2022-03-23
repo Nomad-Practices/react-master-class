c1f3fa7824007069d6e97b153a634f1154ee645d
sjk <jkshin0903@gmail.com>
2022-03-23 22:32:34 +0900

    useRef hook 학습

--- src/components/DroppableBoard.tsx
+++ src/components/DroppableBoard.tsx
@@ -1,6 +1,7 @@
 import { Droppable } from 'react-beautiful-dnd'
 import { Area, Board, Title } from '../components/styled'
 import DraggableCard from '../components/DraggableCard'
+import { useRef } from 'react'
 
 interface IProps {
    todos: string[]
@@ -8,9 +9,19 @@ interface IProps {
 }
 
 function DroppableBoard({ todos, droppableId }: IProps) {
+   /**
+    * Vue에서 template reference처럼 React에서는 template 내에 html element에 access할 때는 useRef hook을 사용한다.
+    */
+   const inputRef = useRef<HTMLInputElement>(null)
+   function onClick() {
+      inputRef.current?.focus()
+      setTimeout(() => inputRef.current?.blur(), 5000)
+   }
    return (
       <Board>
          <Title>{droppableId}</Title>
+         <input ref={inputRef} type="text" placeholder="grab me" />
+         <button onClick={onClick}>click me</button>
          <Droppable droppableId={droppableId}>
             {(provided, { isDraggingOver, draggingFromThisWith }) => (
                /**
