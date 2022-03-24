0b494e933c81c2ad9d6683b3411be1dc95acc283
sjk <jkshin0903@gmail.com>
2022-03-24 22:54:20 +0900

    variants part1

--- src/App.tsx
+++ src/App.tsx
@@ -18,15 +18,19 @@ const Box = styled(motion.div)`
    border-radius: 15px;
    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
+/**
+ * framer motion으로 구현할 animation을 단계별로 정의한 js 객체를 Variant라고 한다.
+ * variants를 사용하면 motion component에 적용할 props를 간결하게 전달할 수 있다.
+ */
+const variants = {
+   start: { scale: 0 },
+   end: { scale: 1, rotateZ: 360, transition: { type: 'spring', duration: 1 } },
+}
 
 function App() {
    return (
       <Wrapper>
-         <Box
-            initial={{ scale: 0 }}
-            animate={{ scale: 1, rotateZ: 360 }}
-            transition={{ type: 'spring', duration: 1 }}
-         />
+         <Box variants={variants} initial="start" animate="end" />
       </Wrapper>
    )
 }
