77ab9ec87b2557e984b9fe728c7aad0ab96ea4d8
sjk <jkshin0903@gmail.com>
2022-03-24 22:12:48 +0900

    framer-motion 설치 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Wrapper, Box } from './components/styled'
+import { motion } from 'framer-motion'
 
 function App() {
    return (
       <Wrapper>
          <Box />
+         <motion.div></motion.div>
       </Wrapper>
    )
 }
