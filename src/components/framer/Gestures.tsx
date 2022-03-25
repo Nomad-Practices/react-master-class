64feeaf3cd5410b6c9ac6b3f161dcab01eef45f8
sjk <jkshin0903@gmail.com>
2022-03-25 22:29:52 +0900

    gestures~

--- src/components/framer/Gestures.tsx
+++ src/components/framer/Gestures.tsx
@@ -1,4 +1,5 @@
-import { motion } from 'framer-motion'
+import { hover } from '@testing-library/user-event/dist/hover'
+import { motion, Variants } from 'framer-motion'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
@@ -16,10 +17,24 @@ const Box = styled(motion.div)`
    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
 
+const boxVariants: Variants = {
+   hover: {
+      scale: 1.5,
+      rotateZ: 90,
+   },
+   tap: {
+      scale: 1,
+      borderRadius: '100px',
+   },
+}
+
 function Gestures() {
    return (
       <Wrapper>
-         <Box whileHover={{ scale: 2 }} />
+         {/**
+          * motion이 적용된 컴포넌트가 mouse event를 감지할 때는 while~ 시작하는 gesture props를 전달하면 된다.
+          */}
+         <Box variants={boxVariants} whileHover="hover" whileTap="tap" />
       </Wrapper>
    )
 }
