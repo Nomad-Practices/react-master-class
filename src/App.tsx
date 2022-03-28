b28026be5005b3373f3c727685e60405b4e363eb
sjk <jkshin0903@gmail.com>
2022-03-28 22:11:54 +0900

    Header part1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './routes/Home'
+import Tv from './routes/Tv'
+import Search from './routes/Search'
+import Header from './components/Header'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
