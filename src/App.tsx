239cc36aa031aa842661af237043841b3bddb357
sjk <jkshin0903@gmail.com>
2022-04-04 21:10:01 +0900

    #2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import styled from 'styled-components'
+
+const Father = styled.div`
+  display: flex;
+`
+
+interface IBoxProps {
+  bgColor?: string
+}
+
+const Box = styled.div<IBoxProps>`
+  background-color: ${(props) => props.bgColor ?? 'black'};
+  width: 100px;
+  height: 100px;
+`
+
+const Text = styled.span`
+  color: white;
+`
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <Father>
+      <Box bgColor="tomato" />
+      <Box bgColor="teal" />
+      <Box>
+        <Text>Hello World</Text>
+      </Box>
+    </Father>
+  )
 }
 
 export default App
