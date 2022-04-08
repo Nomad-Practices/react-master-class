b931176f9a2c45688ea0e6d59c49ab80fa4653bb
sjk <jkshin0903@gmail.com>
2022-04-09 00:10:01 +0900

    홈 버튼 기능 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { ThemeProvider, createGlobalStyle } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import { RecoilRoot } from 'recoil'
-import Router from './Router'
 import { darkTheme, lightTheme } from './theme'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { HelmetProvider } from 'react-helmet-async'
 
 const queryClient = new QueryClient()
-
-/**
- *  앱 전반에 적용할 style은 createGlobalStyle을 사용하면 된다.
- */
-const GlobalStyle = createGlobalStyle`
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-* {
-  box-sizing: border-box;
-}
-body {
-  font-weight: 300;
-  font-family: 'Quicksand', sans-serif;
-  font-family: 'Raleway', sans-serif;
-  background-color: ${(props) => props.theme.backgroundColor};
-  color: ${(props) => props.theme.accentColor};
-  line-height: 1.2;
-}
-a {
-  text-decoration:none;
-  color:inherit;
-}
-`
 
 /**
  * styled components로 앱의 테마를 ThemeProvider로 구현할 수 있다.
@@ -70,8 +24,6 @@ ReactDOM.render(
         <QueryClientProvider client={queryClient}>
           <ThemeProvider theme={darkTheme}>
             <App />
-            <Router />
-            <GlobalStyle />
           </ThemeProvider>
           {/**
            * ReactQueryDevTools => cache에 저장된 query들과 응답 데이터를 확인할 수 있는 devTool
--- src/layout/GlobalLayout.tsx
+++ src/layout/GlobalLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -22,7 +22,9 @@ function GlobalLayout() {
     <>
       <Container>
         <Header>
-          <Title>Crypto Currency</Title>
+          <Link to="/">
+            <Title>Crypto Tracker</Title>
+          </Link>
         </Header>
         <Outlet />
       </Container>
