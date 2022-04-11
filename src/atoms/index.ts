789e9922b94cdb7e416662f9793caab6aebfe61e
sjk <jkshin0903@gmail.com>
2022-04-11 22:29:23 +0900

    #5.11

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,6 +1,17 @@
 import { atom } from 'recoil'
 
-export const isDarkAtom = atom({
+interface ITodo {
+  id: number
+  text: string
+  status: 'TODO' | 'DOING' | 'DONE'
+}
+
+export const isDarkAtom = atom<boolean>({
   key: 'isDark',
   default: true,
+})
+
+export const todoStateAtom = atom<ITodo[]>({
+  key: 'todo',
+  default: [],
 })
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,24 +1,40 @@
 import { useForm } from 'react-hook-form'
+import { todoStateAtom } from '../atoms'
+import { useRecoilState } from 'recoil'
 
 interface IForm {
   todo: string
 }
 
 function TodoList() {
+  const [todoState, setTodoState] = useRecoilState(todoStateAtom)
   const { register, handleSubmit, setValue } = useForm<IForm>()
-  function onValid(data: IForm) {
+  function onValid({ todo }: IForm) {
+    setTodoState((prev) => [
+      ...prev,
+      { id: Date.now(), text: todo, status: 'TODO' },
+    ])
     setValue('todo', '')
   }
   return (
-    <form onSubmit={handleSubmit(onValid)}>
-      <input
-        {...register('todo', {
-          required: 'please write sth',
-        })}
-        type="text"
-      />
-      <button>Add</button>
-    </form>
+    <>
+      <h1>To dos</h1>
+      <hr />
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register('todo', {
+            required: 'please write sth',
+          })}
+          type="text"
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {todoState.map((td) => (
+          <li key={td.id}>{td.text}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
