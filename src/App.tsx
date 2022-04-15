6e8988548b7a506cbb4da836525c2422680ef102
sjk <jkshin0903@gmail.com>
2022-04-16 01:11:13 +0900

    #7.15

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,7 @@ const Wrapper = styled(motion.div)`
 `
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   display: flex;
@@ -21,13 +20,37 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
 
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
-  border-radius: 50px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
 `
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`
+
+const overlayVariants: Variants = {
+  initial: {
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+  },
+  animate: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  exit: {
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+  },
+}
 
 function App() {
   const [clicked, setClicked] = useState(false)
@@ -39,12 +62,24 @@ function App() {
    */
   return (
     <Wrapper onClick={toggleClicked}>
-      <Box>
-        {!clicked && <Circle layoutId="circle" style={{ borderRadius: 50 }} />}
-      </Box>
-      <Box>
-        {clicked && <Circle layoutId="circle" style={{ borderRadius: 0 }} />}
-      </Box>
+      <Grid>
+        <Box layoutId="hello" />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
+      <AnimatePresence>
+        {clicked && (
+          <Overlay
+            variants={overlayVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+          >
+            <Box layoutId="hello" style={{ width: 400, height: 200 }} />
+          </Overlay>
+        )}
+      </AnimatePresence>
     </Wrapper>
   )
 }
